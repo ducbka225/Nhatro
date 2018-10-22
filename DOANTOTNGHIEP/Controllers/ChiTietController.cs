7098cb4bfff656e1c63d0d90a9c5d26a6173e3e8
Nguyen Van Duc <duc.nguyenvan@vietis.com.vn>
2018-10-22 15:01:12 +0700

    layout

---
+++ DOANTOTNGHIEP/Controllers/ChiTietController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOANTOTNGHIEP.Controllers
+{
+    public class ChiTietController : Controller
+    {
+        // GET: ChiTiet
+        public ActionResult ChiTiet()
+        {
+            return View();
+        }
+    }
+}
---
+++ DOANTOTNGHIEP/Controllers/PhongTroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOANTOTNGHIEP.Controllers
+{
+    public class PhongTroController : Controller
+    {
+        // GET: PhongTro
+        public ActionResult PhongTro()
+        {
+            return View();
+        }
+    }
+}
