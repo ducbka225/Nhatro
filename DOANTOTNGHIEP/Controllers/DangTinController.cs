d2dc5061c89053424d32631108c681ea0c652422
Nguyen Van Duc <duc.nguyenvan@vietis.com.vn>
2018-10-22 17:31:51 +0700

    update layout

---
+++ DOANTOTNGHIEP/Controllers/DangTinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOANTOTNGHIEP.Controllers
+{
+    public class DangTinController : Controller
+    {
+        // GET: DangTin
+        public ActionResult DangTin()
+        {
+            return View();
+        }
+    }
+}
