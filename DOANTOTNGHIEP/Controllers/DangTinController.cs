e8b43df3db3fe6f4060167bdc2ccd8d1180f41df
Ducbka <nguyenvanduc95bg@gmail.com>
2018-11-08 22:29:25 +0700

    08-11

--- DOANTOTNGHIEP/Controllers/DangTinController.cs
+++ DOANTOTNGHIEP/Controllers/DangTinController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DOANTOTNGHIEP.Models;
+using DOANTOTNGHIEP.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,37 @@ namespace DOANTOTNGHIEP.Controllers
 {
     public class DangTinController : Controller
     {
+        NhaTroEntities db = new NhaTroEntities();
         // GET: DangTin
         public ActionResult DangTin()
         {
             return View();
+        }
+
+        public ActionResult GetDistrict(int provinceid)
+        {
+            var dataResult = (from d in db.District
+                              join p in db.Province on d.IdProvince equals p.Id
+                              where p.Id == provinceid
+                              select new DistrictModel
+                              {
+                                  Id = d.Id,
+                                  Name = d.Name,
+                              }).ToList();
+            return Json(dataResult, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetStreet(int districtId)
+        {
+            var dataResult = (from s in db.Street
+                              join d in db.District on s.IdDistrict equals d.Id
+                              where d.Id == districtId
+                              select new StreetModel
+                              {
+                                  Id = s.Id,
+                                  Name = s.Name,
+                              }).ToList();
+            return Json(dataResult, JsonRequestBehavior.AllowGet);
         }
     }
 }
